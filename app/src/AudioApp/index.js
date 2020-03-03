8ac1f87dc5c92ff3f55b5dbbd6197e75fa7ca06c
Muhammad Anshad <muhammad.anshad@teronext.com>
2020-03-03 12:53:32 +0530

    Added control icons using material UI

--- app/src/AudioApp/index.js
+++ app/src/AudioApp/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { PlayCircleFilled, Stop, VolumeUp, AddCircle, RemoveCircle } from '@material-ui/icons';
 import audio from '../Assets/Audio/song.mp3';
 
 export const AudioApp = () => {
@@ -8,10 +9,34 @@ export const AudioApp = () => {
             id='player'
             controls
         />
-    )
+    );
+
+    const renderPlayerControls = () => (
+        <div id="player-controls">
+            <div id="play-pause">
+                <PlayCircleFilled />
+            </div>
+            <div id="stop">
+                <Stop />
+            </div>
+            <div id="mute">
+                <VolumeUp />
+            </div>
+            <div id="volume-plus">
+                <AddCircle />
+            </div>
+            <div id="volume-minus">
+                <RemoveCircle />
+            </div>
+        </div>
+    );
+
     return (
         <div className="audio-app">
-            <div id="audio-player-card">{renderAudio()}</div>
+            <div id="audio-player-card">
+                {renderAudio()}
+                {renderPlayerControls()}
+            </div>
         </div>
     );
 };
